498fc75e78d680da0d5f3389a7eabc5ea1c2f227
Odette Morentin <sandraodettems@gmail.com>
2025-01-31 14:59:18 -0600

    Se agregó la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 let amigos = []; //Almacena los nombres de los amigos
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value; //Obtiene el input del nombre del amigo
+    //Validación casilla vacía
+    if (nombreAmigo == ""){
+        alert("Debes ingresar un nombre");
+    } else {
+        amigos.push(nombreAmigo);
+    }
+    console.log(nombreAmigo)
+
+    //Limpiar espacio
+    document.querySelector("#amigo").value = ""; 
+}
