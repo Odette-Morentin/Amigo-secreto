c22cb1ebe344b582a606acac70a3c8209c3f3451
Odette Morentin <sandraodettems@gmail.com>
2025-01-31 16:29:27 -0600

    Función que muestra la lista

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ let amigos = []; //Almacena los nombres de los amigos
 
 function agregarAmigo(){
     let nombreAmigo = document.getElementById("amigo").value; //Obtiene el input del nombre del amigo
+
     //Validación casilla vacía
     if (nombreAmigo == ""){
         alert("Debes ingresar un nombre");
     } else {
         amigos.push(nombreAmigo);
     }
-    console.log(nombreAmigo)
 
     //Limpiar espacio
     document.querySelector("#amigo").value = ""; 
+    recorrerArray();
+}
+
+function recorrerArray(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; //Borra el contenido de la lista
+    //Recorre el array de amigos
+    for ( let i = 0; i<amigos.length; i++){
+        let item = document.createElement("li"); //Crea un elemento li
+        item.textContent = amigos[i]; //Asigna el texto del item
+        lista.appendChild(item); //Agrega el item a la lista
+    }
 }
